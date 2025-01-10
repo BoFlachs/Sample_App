5377fcae46e93aebcfc8acdfe440d11c9dd4f790
Bo Flachs <bflachs@sogyo.nl>
2025-01-10 13:25:00 +0100

    Removed activation tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -49,15 +49,15 @@ class AccountActivationTest < UsersSignup
   #   assert_not is_logged_in?
   # end
 
-  test 'should not be able to log in with invalid activation token' do
-    get edit_account_activation_path('invalid token', email: @user.email)
-    assert_not is_logged_in?
-  end
+  # test 'should not be able to log in with invalid activation token' do
+  #   get edit_account_activation_path('invalid token', email: @user.email)
+  #   assert_not is_logged_in?
+  # end
 
-  test 'should not be able to log in with invalid email' do
-    get edit_account_activation_path(@user.activation_token, email: 'wrong')
-    assert_not is_logged_in?
-  end
+  # test 'should not be able to log in with invalid email' do
+  #   get edit_account_activation_path(@user.activation_token, email: 'wrong')
+  #   assert_not is_logged_in?
+  # end
 
   # test 'should log in successfully with valid activation token and email' do
   #   get edit_account_activation_path(@user.activation_token, email: @user.email)
